cd402733d0c3cbae6040f0a64c888a42ecec94e5
StarkOp23 <sanjumaiti.maligram@gmail.com>
2023-10-11 12:46:49 +0530

    navbar

---
+++ netflix_ui/src/App.jsx
@@ -0,0 +1,13 @@
+import Home from './home/Home'
+import React from 'react'
+import './App.scss'
+
+const App = () => {
+    return (
+        <div>
+            <Home />
+        </div>
+    )
+}
+
+export default App
